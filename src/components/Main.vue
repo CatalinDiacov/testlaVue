bbd0526f0159a966421e1ff7cda36039fa594e7d
DiacovCatalin <90838170+DiacovCatalin@users.noreply.github.com>
2023-03-21 12:40:53 +0200

    Initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="inputs">
+    <p>Message is: {{ nume }}</p>
+    nume <input v-model="nume"><br>
+    pret <input v-model="pret"><br>
+    cantitate <input v-model="cantitate"><br>
+    <button type="submit" @click="addProduct">Add Product</button>
+    <button type="submit" @click="Reset">Reset</button>
+  </div>
+</template>
+
---
+++ src/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+  <table >
+    <thead>
+    <tr>
+      <th>Nr</th>
+      <th>Numele Produsului</th>
+      <th>Pretul unei cantitati</th>
+      <th>Numarul de cantitati</th>
+      <th>Pretul total</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <tr v-for="product in products">
+      <td > index </td>
+      <td >{{ product.nume }}</td>
+      <td >{{ product.pret }}</td>
+      <td >{{ product.cantitate }}</td>
+      <td >{{ product.totalul }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+      products:[{
+        nume: 'catalin',
+        pret: 12,
+        cantitate: 2,
+        totalul: 24
+      }]
+</script>
